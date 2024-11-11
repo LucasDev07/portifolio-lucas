f5e9ff76dd08e8c16e01d19f7142b3d257483b1a
Lucas Vargas <lukeamfernandes@gmail.com>
2024-11-11 15:01:50 -0300

    menu do topo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ProfileService } from '../services/menu.service';
+import { ProfileService } from '../services/profile.service';
 
 @Component({
   selector: 'app-home',
